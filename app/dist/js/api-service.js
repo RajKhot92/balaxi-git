102c01bfe3c1ac8104c385961cbf699e1ce597bd
RajKhot92 <khot.raj92@gmail.com>
2022-05-06 19:09:13 +0530

    Initial Project Version

---
+++ app/dist/js/api-service.js
@@ -0,0 +1,20 @@
+// const serverURL = "http://localhost/balaxi/api/";
+const serverURL = "https://klugerkopf.com/balaxi/api/";
+
+function callAPIService(url, formData){
+	let response = null;
+    $.ajax({
+        url: serverURL+url,
+        type: "GET",
+        async: false,
+        cache: false,
+        data: formData,
+        success: function(result) {
+            response = result;
+        },
+        error: function() {
+            response = "API service is not responding. Please try again.";
+        }
+    });
+    return response;
+}
